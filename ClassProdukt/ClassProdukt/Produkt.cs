cec3498bbe6c8174d2f51bd673c3d828c655e210
Farid477 <faridrn-af106@code.edu.az>
2024-10-15 01:57:55 +0400

    ClassProdukt

---
+++ ClassProdukt/ClassProdukt/Produkt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProdukt
+{
+    public class Produkt
+    {
+        public string ad;
+        public string brendad;
+        public double qiymet;
+        public Produkt(string ad, string brendad, double qiymet)
+        {
+            this.ad = ad;
+            this.brendad = brendad;
+            this.qiymet = qiymet;
+        }
+    }
+}
+
---
+++ ClassProdukt/ClassProdukt/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ClassProdukt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+            Produkt corek = new Produkt("UnMehsulu", "LezgiCoreyi", 1.20);
+            Produkt icki = new Produkt("Araq", "Samaqon", 19.99);
+            Produkt kofe = new Produkt("Qehve", "Nescafe", 10);
+            Produkt pesok = new Produkt("SekerTozu", "AzerSeker", 5);
+            Produkt asvijitel = new Produkt("AirWick", "Glade", 13.6);
+
+            Produkt[] produkts = { pesok, kofe, icki, asvijitel, corek};
+            
+            double ucuz = double.Parse(Console.ReadLine());
+            double baha = double.Parse(Console.ReadLine());
+            
+            Qenaet(produkts, ucuz, baha);
+
+        }
+        static void Qenaet(Produkt[] products, double ucuz, double baha)
+        {
+        bool tapildi = false;
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (ucuz < products[i].qiymet && products[i].qiymet < baha)
+                {
+                    Console.WriteLine(products[i].brendad + " " + products[i].qiymet);
+                    tapildi = true;
+                }
+            }
+            if (!tapildi)
+            {
+                Console.WriteLine("Mehsul tapilmadi!");
+            }
+        }
+    }
+}
