2dc52749cc8d27dc42ef8efef8d74f511ff913be
Farid477 <faridrn-af106@code.edu.az>
2024-10-20 22:52:49 +0400

    ModelDLL

---
+++ ModelDLL/ClassLibrary1/Class1.cs
@@ -0,0 +1,55 @@
+﻿namespace ClassLibrary1;
+
+public class Manager
+{
+    protected Employee GetPromotion(Employee employee)
+    {
+        employee.Salary += 100;
+        return employee;
+    }
+}
+
+public class Assistant : Manager
+{
+    public Employee GetFeedback(Employee employee)
+    {
+        if (employee.IsSuccessfull)
+        {
+            GetPromotion(employee);
+        }
+        return employee;
+    }
+}
+
+public class Employee
+{
+    private string _name;
+    private double _salary;
+    private bool _isSuccessfull = false;
+
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public double Salary
+    {
+        get { return _salary; }
+        set { _salary = value; }
+    }
+
+    public bool IsSuccessfull
+    {
+        get { return _isSuccessfull; }
+        set { _isSuccessfull = value; }
+    }
+
+
+    public Employee(string name, double salary)
+    {
+        Name = name;
+        Salary = salary;
+    }
+}
---
+++ ModelDLL/ModelDLL/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+
+namespace ModelDLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Ferid", 1500);
+            Assistant assistant = new Assistant();
+
+            assistant.GetFeedback(employee);
+            Console.WriteLine(employee.Salary);
+
+            employee.IsSuccessfull = true;
+            assistant.GetFeedback(employee);
+            Console.WriteLine(employee.Salary);
+        }
+    }
+}
