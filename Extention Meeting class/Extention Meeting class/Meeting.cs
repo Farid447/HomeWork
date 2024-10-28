a82f0c171f06b86eea5527b171f66ff90747d899
Farid477 <faridrn-af106@code.edu.az>
2024-10-29 02:54:08 +0400

    Exception Task

---
+++ Extention Meeting class/Extention Meeting class/Meeting.cs
@@ -0,0 +1,57 @@
+﻿namespace Extention_Meeting_class;
+
+internal class Meeting
+{
+    private string _fullname;
+    private DateTime _begin;
+    private DateTime _end;
+    public string FullName { get { return _fullname; } set { _fullname = value; } }
+    public DateTime Begin { get { return _begin; } set { _begin = value; } }
+    public DateTime End { get { return _end; } set { _end = value; } }
+    public Meeting(string fullName, DateTime begin, DateTime end)
+    {
+        FullName = fullName;
+        Begin = begin;
+        End = end;
+    }
+}
+
+static class MeetingSchedule
+{
+    public static Meeting[] Meetings = new Meeting[0];
+
+    public static void SetMeeting(string fullName, DateTime begin, DateTime end)
+    {
+        if (begin >= end)
+        {
+            throw new WrongDateIntervalException();
+        }
+
+        for (int i = 0; i < Meetings.Length; i++)
+        {
+            if ((begin > Meetings[i].Begin && begin < Meetings[i].End) || (end < Meetings[i].End && end > Meetings[i].Begin) || (begin <= Meetings[i].Begin && end >= Meetings[i].End))
+            {
+                throw new ReservedDateIntervalException();
+            }
+        }
+        Array.Resize(ref Meetings, Meetings.Length + 1);
+        Meetings[^1] = new Meeting(fullName, begin, end);
+    }
+}
+
+
+internal class ReservedDateIntervalException : Exception
+{
+    public ReservedDateIntervalException() : base("bu meeting basqa meetingle toqqusur")
+    {
+
+    }
+}
+
+internal class WrongDateIntervalException : Exception
+{
+    public WrongDateIntervalException() : base("daxil etdiyiniz tarix yanlisdir")
+    {
+
+    }
+}
---
+++ Extention Meeting class/Extention Meeting class/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Extention_Meeting_class;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MeetingSchedule.SetMeeting("Murad Cavadov", new DateTime(2024, 10, 28), new DateTime(2024, 10, 29));
+
+        MeetingSchedule.SetMeeting("Revan Bayramov", new DateTime(2024, 10, 27), new DateTime(2024, 10, 28));
+
+        MeetingSchedule.SetMeeting("Ferid Necefov", new DateTime(2024, 10, 27), new DateTime(2024, 10, 29));
+
+    }
+}
