fa853cf987730d06a7afc04bb5e500e0b4c929e9
Farid477 <faridrn-af106@code.edu.az>
2024-11-04 00:32:29 +0400

    FileStream

---
+++ FileStream/FileStream/FileHelper.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Linq;
+
+namespace FileStream;
+internal class FileHelper
+{
+    List<string> list  = [];
+    string str;
+
+    public string path { get; set; }
+    public FileHelper(string path)
+    {
+        this.path = path;
+    }
+    public void Add(string name)
+    {
+        using StreamReader sr = new StreamReader(path);
+
+        list = JsonConvert.DeserializeObject<List<string>>(sr.ReadToEnd());
+        
+        list.Add(name);
+        
+        sr.Close();
+        
+        using StreamWriter sw = new StreamWriter(path, false);
+        sw.Write(JsonConvert.SerializeObject(list));
+    }
+    public bool Exist(string name)
+    {
+        using StreamReader sr = new StreamReader(path);
+
+        list = JsonConvert.DeserializeObject<List<string>>(sr.ReadToEnd());
+
+        return list.Contains(name);
+    }
+    public void Update(int index, string name)
+    {
+        if (index >= 0 && index < list.Count)
+        {
+            using StreamReader sr = new StreamReader(path);
+
+            list = JsonConvert.DeserializeObject<List<string>>(sr.ReadToEnd());
+
+            list[index] = name;
+
+            sr.Close();
+
+            using StreamWriter sw = new StreamWriter(path, false);
+            sw.Write(JsonConvert.SerializeObject(list));
+        }
+        else
+        {
+            Console.WriteLine("indexi duzgun daxil edin");
+        }
+    }
+    public void Delete(int index)
+    {
+        if (index >= 0 && index < list.Count)
+        {
+            using StreamReader sr = new StreamReader(path);
+
+            list = JsonConvert.DeserializeObject<List<string>>(sr.ReadToEnd());
+
+            list.RemoveAt(index);
+
+            sr.Close();
+
+            using StreamWriter sw = new StreamWriter(path, false);
+            sw.Write(JsonConvert.SerializeObject(list));
+        }
+        else
+        {
+            Console.WriteLine("indexi duzgun daxil edin");
+        }
+    }
+}
---
+++ FileStream/FileStream/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FileStream;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\Users\Farid\Desktop\FileStream\FileStream\names.json";
+        FileHelper help = new(path);
+
+        help.Add("murad");
+        help.Add("ferid");
+
+        help.Delete(0);
+        help.Update(1, "revan");
+        Console.WriteLine(help.Exist("huseyn"));
+        //help.Exist("revan");
+    }
+}
